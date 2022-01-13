78ee9894ccc91683a5c75d8a791e1683bfb4b179
Danylo79 <dan.komisarenko@gmail.com>
2022-01-13 11:17:14 -0500

    Item Reference

--- src/main/java/dev/dankom/pi/PrimevalItems.java
+++ src/main/java/dev/dankom/pi/PrimevalItems.java
@@ -3,8 +3,6 @@ package dev.dankom.pi;
 import org.bukkit.NamespacedKey;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import javax.naming.event.NamespaceChangeListener;
-
 public final class PrimevalItems extends JavaPlugin {
     @Override
     public void onEnable() {
